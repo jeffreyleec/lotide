8560ea66bbf6a52a6f6373f0d2e3a863f7f3c0ca
Jeffrey <jeffrey-lee@live.ca>
2022-07-25 23:30:49 +0000

    reverse js

---
+++ reverse.js
@@ -0,0 +1,20 @@
+const args = process.argv;
+
+let reverse = function(input) {
+  let raw = "";
+  for (let w = 2; w < input.length; w++) {
+    if (input[w].length > 1) {
+      let innie = "";
+      for (let l = input[w].length - 1; l >= 0; l--) {
+        innie += input[w][l];
+      }
+      raw += `${innie} \n `;
+       
+    } else {
+      raw += `${input[w]} \n `;
+    }
+  }
+  return raw;
+};
+
+console.log(reverse(args));
