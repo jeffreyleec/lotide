61dd4dcfe21a7d6e1c5c6cbd5889ae5366ba36bf
Jeffrey <jeffrey-lee@live.ca>
2022-07-26 19:38:00 +0000

    eqArrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🟢🟢🟢  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🟢🟢🟢 Assertion Failed: ${actual} !== ${expected}`);
-  }
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+ }
 };
 
 // TEST CODE
---
+++ eqArrays.js
@@ -0,0 +1,17 @@
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+  
+  return console.log(`🟢🟢🟢  Assertion Passed: ${actual} === ${expected}`);
+};
+
+
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
