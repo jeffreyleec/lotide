48edd545712bb626799c070153b135adef814644
Jeffrey <jeffrey-lee@live.ca>
2022-07-27 20:25:54 +0000

    fixed eqObjects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,49 +1,49 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log( `🟢🟢🟢  Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🟢🟢🟢  Assertion Passed: ${actual} === ${expected}`);
 
   } else {
-    console.log( `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
   
 };
 
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
 
-  const keys1 = Object.keys(object1).sort()
-  const keys2 = Object.keys(object2).sort()
-//console.log("\nstart")
+  const keys1 = Object.keys(object1).sort();
+  const keys2 = Object.keys(object2).sort();
+  //console.log("\nstart")
   //console.log(keys1, keys2)
 
   if (!eqArrays(keys1, keys2)) {
-    return false
+    return false;
   }
 
   for (const key of keys1) {
-    const value1 = object1[key]
-    const value2 = object2[key]
+    const value1 = object1[key];
+    const value2 = object2[key];
 
     //console.log(value1, value2)
 
 
-    if (Array.isArray(value1) ) {
+    if (Array.isArray(value1)) {
       if (!eqArrays(value1, value2)) {
         //console.log("eq arry failed!!!!")
-        return false
+        return false;
       }
-      continue
+      continue;
     }
 
 
     if (value1 !== value2) {
     //console.log("value check fail")
-      return false
+      return false;
     }
 
     
   }
-  return true
-}
+  return true;
+};
 
 
 const eqArrays = function(actual, expected) {
@@ -59,36 +59,16 @@ const eqArrays = function(actual, expected) {
   
 };
 
-const assertArraysEqual = function(actual, expected) {
-  if(eqObjects(actual, expected) === true) {
-    return console.log(`🟢🟢🟢  Assertion Passed: ${actual} === ${expected}`)
-     
-  } else{
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-     
 
-  }
+const assertObjectsEqual = function(object1, object2) {
+  const result = eqObjects(object1,object2);
+  console.log(object1, object2, result);
+  assertEqual(result,true);
 };
 
 
 
+const object1 = { a: '1', b: 2 };
+const object2 = { b: 2, a: '1' };
 
-
-const assertObjectsEqual = function(object1, object2) {
-  const result = eqObjects(object1,object2)
-  console.log(object1, object2, result)
-  assertEqual(result,true);
-}
-
-
-
-
-//✅✅✅ Assertion Passed: [object Object] === [object Object]
-//🛑🛑🛑 Assertion Failed: [object Object] !== [object Object]
-
-
-
-const object1 = { a: '1', b: 2 }  
-const object2 = { b: 2, a: '1' }
-
-assertObjectsEqual(object1,object2)
+assertObjectsEqual(object1,object2);
--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return `🟢🟢🟢  Assertion Passed: ${actual} === ${expected}`;
 
@@ -8,7 +8,7 @@ const assertEqual = function (actual, expected) {
 
 };
 
-const eqArrays = function (actual, expected) {
+const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
   }
@@ -21,42 +21,42 @@ const eqArrays = function (actual, expected) {
 
 };
 
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
 
-  const keys1 = Object.keys(object1).sort()
-  const keys2 = Object.keys(object2).sort()
-//console.log("\nstart")
+  const keys1 = Object.keys(object1).sort();
+  const keys2 = Object.keys(object2).sort();
+  //console.log("\nstart")
   //console.log(keys1, keys2)
 
   if (!eqArrays(keys1, keys2)) {
-    return false
+    return false;
   }
 
   for (const key of keys1) {
-    const value1 = object1[key]
-    const value2 = object2[key]
+    const value1 = object1[key];
+    const value2 = object2[key];
 
     //console.log(value1, value2)
 
 
-    if (Array.isArray(value1) ) {
+    if (Array.isArray(value1)) {
       if (!eqArrays(value1, value2)) {
         //console.log("eq arry failed!!!!")
-        return false
+        return false;
       }
-      continue
+      continue;
     }
 
 
     if (value1 !== value2) {
     //console.log("value check fail")
-      return false
+      return false;
     }
 
     
   }
-  return true
-}
+  return true;
+};
 
 
 
